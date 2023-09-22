372baf27401717571adfc2089c10570faaa074eb
JAEYOUNG <JAEYOUNG@DESKTOP-3LJTQ9L>
2023-09-22 16:54:59 +0900

    사용자가 2개의 숫자를 입력해서 전송하면 /quiz/result.jsp 화면이 열리고 여기에는 두개의 숫자를 모두 더해서 보내온값을 출력하는 화면 (단, 두개의 숫자를 모두더하는 작업은 MyLogic3 클래스를 이용해서 처리)

---
+++ quiz1_ljy/src/main/java/edu/mit/service/QuizService.java
@@ -0,0 +1,27 @@
+package edu.mit.service;
+
+import org.springframework.stereotype.Service;
+
+import edu.mit.dao.TwoVO;
+import edu.mit.logic.MyLogic;
+import edu.mit.logic.MyLogic3;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
+public class QuizService {
+	
+	  private MyLogic3 logic3;
+	
+		//두수를 모두 더해서 돌려주는 서비스
+	  public int allsum(TwoVO vo) {
+		    return MyLogic.addUp(vo.getNum1(), vo.getNum2());		  
+	  }
+    
+    
+	
+	
+	
+	
+    
+}
